c9ec456f668e8b04ada5db4c1ab6372b16f09cdc
Mufti Faris Murtadho <muftifarisjob@gmail.com>
2026-09-06 02:26:39 +0700

    feat(privacy): enhance microphone control with pipewire integration and level monitoring

--- Code/privacy/include/microphone-control.h
+++ Code/privacy/include/microphone-control.h
@@ -5,6 +5,8 @@
 #include <QString>
 #include <QObject>
 #include <QList>
+#include <QTimer>
+#include <QMap>
 
 class MicrophoneControl : public QObject
 {
@@ -23,25 +25,37 @@ public:
     void setGlobalMicrophoneEnabled(bool enabled);
     bool isGlobalMicrophoneEnabled() const;
 
-    // Status
+    // Status & active recording session tracking
     bool isMicrophoneInUse() const;
     QString getCurrentMicrophoneUser() const;
     QList<QString> getAppsWithMicrophoneAccess();
+    QList<QString> getActiveRecordingApps() const;
 
     // Audio monitoring
     int getMicrophoneLevel() const;
     void enableAudioMonitoring(bool enable);
+
+public slots:
+    void updateMicrophoneState();
+    void onPermissionChangedSlot(const QString& appId, int categoryInt, int stateInt);
 
 signals:
     void microphoneAccessRequested(const QString& appId, const QString& reason);
     void microphoneStatusChanged(bool inUse, const QString& appId);
     void audioLevelChanged(int level);
     void permissionChanged(const QString& appId, int state);
+    void globalMicrophoneToggled(bool enabled);
 
 private:
     PrivacyManager* m_privacyManager;
     bool m_globalEnabled;
     bool m_monitoringEnabled;
+    int m_currentLevel;
+    QTimer* m_monitoringTimer;
+    QMap<QString, qint64> m_activeStreams;
+
+    void updateAudioLevel();
+    void checkActiveRecordingStreams();
 };
 
 #endif // MICROPHONE_CONTROL_H
